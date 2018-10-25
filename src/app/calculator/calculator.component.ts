12f46cc5d3784f70abb132abb6809fb89f56bd9a
Marwan El-Adawy <marwan.mahmoud@cis.asu.edu.eg>
2018-10-25 20:17:17 +0200

    Calculator

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  public number1: number;
+  public number2: number;
+  public result: number;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  public add() {
+    this.result = this.number1 + this.number2;
+}
+}
